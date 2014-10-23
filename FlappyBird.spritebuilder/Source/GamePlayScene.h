df60f73ad9228bf551fbd84832294beaaceeca55
Asirisss <asirisss87@gmail.com>
2014-10-23 19:04:31 +0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.h
+++ FlappyBird.spritebuilder/Source/GamePlayScene.h
@@ -17,6 +17,9 @@ typedef NS_ENUM(NSInteger, DrawingOrder) {
 
 @interface GamePlayScene : CCNode <CCPhysicsCollisionDelegate>
 {
+    Character* character;
+    CCPhysicsNode* physicsNode;
+
     // define variables here;
 }
 
